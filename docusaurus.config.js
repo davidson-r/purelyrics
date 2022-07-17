3b76b1a9e5ca399a145554b80198c990439bcb86
Jeba <davidson.yyz@gmail.com>
2022-07-16 22:09:22 -0400

    config changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,16 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: "PureLyrics",
   tagline: "Home to Tamil and English Christian lyrics",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
+  url: "https://davidson-r.github.io",
+  baseUrl: "/purelyrics/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   // favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "davidson-r", // Usually your GitHub org/user name.
+  projectName: "purelyrics", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
